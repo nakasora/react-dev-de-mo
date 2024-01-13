ff9756dbe1cdd02340abbd9eb10d6832b8d878df
github nakasora <n17302826@gmail.com>
2024-01-13 09:23:08 +0900

    refactor: セミコロン除外

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 /** @type {import("prettier").Config} */
 const config = {
-  semi: true,
+  semi: false,
   tabWidth: 2,
   singleQuote: true,
-};
+}
 
-export default config;
+export default config
